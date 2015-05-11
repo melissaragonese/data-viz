dc3de13d6848fb7178692c5d7a2d3760479c3888
melissaragonese <melissaragonese92@gmail.com>
2015-05-11 01:49:26 -0400

    adding remaining files

---
+++ Data-Viz-Project/HTMLs and d3 stuff/website.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Data Visualizations</title>
+    <link rel="stylesheet" type="text/css" href="../../css/styles.css"/>
+    <script type="text/javascript" src="../../lib/d3.js"></script>
+</head>
+
+<body>
+
+<div class='log'>
+<a href="stocks.html">Link 1</a>
+</div>
+
+</body>
+
+</html>
